a3b579adffcda9073fc7cf76e6caa09d81768b9d
JoyceLA <joycseleneanaid@gmail.com>
2014-11-04 13:49:40 -0600

    Update steps.py

--- AceptanceTest/test/features/steps.py
+++ AceptanceTest/test/features/steps.py
@@ -9,26 +9,26 @@ def given_i_have_the_number1_number2(step, number1, number2):
     world.number = [int(number1),int(number2)]
 
 @step(u'Given I have the number "([^"]*)" and "([^"]*)"')
-def given_i_have_the_number1_number2(step, number1, number2):
+def given_i_have_the_number1_number2_float(step, number1, number2):
     world.number = [float(number1),float(number2)]
 
 @step(u'When I compute its plus')
-def when_i_compute_its_factorial(step):
+def when_i_compute_its_plus(step):
 	cal = Calculator()
 	world.number = cal.summa(world.number[0],world.number[1])
 	
 @step(u'When I compute its subtraction')
-def when_i_compute_its_factorial(step):
+def when_i_compute_its_subtraction(step):
 	cal = Calculator()
 	world.number = cal.resta(world.number[0],world.number[1])
 
 @step(u'When I compute its multiplication')
-def when_i_compute_its_factorial(step):
+def when_i_compute_its_multiplication(step):
 	cal = Calculator()
 	world.number = cal.multiplica(world.number[0],world.number[1])
 
 @step(u'When I compute its division')
-def when_i_compute_its_factorial(step):
+def when_i_compute_its_division(step):
 	cal = Calculator()
 	world.number = cal.divide(world.number[0],world.number[1])
 
@@ -37,6 +37,6 @@ def then_i_see_the_number_1(step, expected):
     assert world.number == int(expected)
 
 @step(u'Then I see the number "([^"]*)"')
-def then_i_see_the_number_1(step, expected):
+def then_i_see_the_number_1_float(step, expected):
     assert float(world.number) == float(expected),"Resultado = {0} and {1}".format(world.number,expected)
 
